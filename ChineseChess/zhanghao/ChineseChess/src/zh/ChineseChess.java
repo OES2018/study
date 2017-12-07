989930ea7b7a735b71d588897536d43342a7bd14
张浩 <34115879+SYSUzhanghao86@users.noreply.github.com>
2017-12-07 22:10:21 +0800

    Add files via upload

---
+++ ChineseChess/zhanghao/ChineseChess/src/zh/ChineseChess.java
@@ -0,0 +1,13 @@
+package zh;
+
+import java.io.IOException;
+
+public class ChineseChess {
+
+	public static void main(String[] args) throws IOException {
+		//初始化棋盘
+		ChessBoard chessBoard=new ChessBoard();
+		chessBoard.init();
+        //移动棋子操作......
+	}
+}
