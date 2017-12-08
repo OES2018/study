9d20cdf2af36e3c5d9c6f28602751b9eebc8cbec
jwZZZ <627878985@qq.com>
2017-12-08 14:38:32 +0800

    Update

--- ChineseChess/zhongjiawei/ChineseChess/src/com/zjw/chineseChess/ChessBoard.java
+++ ChineseChess/zhongjiawei/ChineseChess/src/com/zjw/chineseChess/ChessBoard.java
@@ -2,212 +2,78 @@ package com.zjw.chineseChess;
 
 import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.Image;
-import java.awt.Toolkit;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-import java.awt.event.MouseMotionListener;
 
 import javax.swing.JPanel;
 
+import com.sun.javafx.css.Rule;
+
 /**
- * æ£‹ç›˜ç±»
+ * ÆåÅÌ
  * @author jiawei
  *
  */
-public class ChessBoard extends JPanel implements MouseListener,MouseMotionListener{
-	public ChessPoint point[][];
-	public int unitWidth,unitHeight;
-	private int x,y;
-	private int Xaxis ,Yaxis;
-	private Image img;
-	protected Image pieceImg;
-	private boolean move = false;
-	public String Redcolor = "çº¢æ–¹",BlackColor = "é»‘æ–¹";
-	
-	ChessPiece redcar1,redcar2,redhorse1,redhorse2,redelephant1,redelephant2,redgeneral,redofficial1,redofficial2,
-	redcannon1,redcannon2,redsoldier1,redsoldier2,redsoldier3,redsoldier4,redsoldier5;
-	ChessPiece blackcar1,blackcar2,blackhorse1,blackhorse2,blackelephant1,blackelephant2,blackgeneral,blackofficial1,blackofficial2,
-	blackcannon1,blackcannon2,blacksoldier1,blacksoldier2,blacksoldier3,blacksoldier4,blacksoldier5;
-	
-	int startX,startY;
-	int startI,startJ;
-	public boolean redgo = true ,blackgo = false;
-	Rule rule = null;
-	public MakeChessManual record = null;
-	
-	public ChessBoard (int w,int h,int r,int c){
-		setLayout(null);//é¦–å…ˆå°†å®¹å™¨çš„å¸ƒå±€è®¾ç½®ä¸ºç©ºå¸ƒå±€ï¼›
-		addMouseListener(this);//æ·»åŠ é¼ æ ‡ç›‘å¬å™¨,ç›‘å¬é¼ æ ‡äº‹ä»¶MouseEventï¼›
-		addMouseMotionListener(this);//æ·»åŠ é¼ æ ‡ç›‘å¬å™¨ï¼Œç›‘å¬é¼ æ ‡çš„ç§»åŠ¨å’Œæ‹–æ”¾ï¼›
-		Color bc = getBackground();//èƒŒæ™¯é¢œè‰²ï¼›
-		unitHeight = h;
-		unitWidth = w;
-		Xaxis = r;
-		Yaxis = c;
-		point = new ChessPoint[r+1][c+1];
-		for(int i=1;i<=r;i++){
-			for(int j=1;j<=c;j++){
-				point[i][j]=new ChessPoint(i*unitWidth,j*unitHeight,false);
-			}
-		}
-		
-		//rule = new Rule(this, point);  
-        //record = new MakeChessManual(this, point);  
-  
-        img = Toolkit.getDefaultToolkit().getImage("board.jpg");  
-        pieceImg = Toolkit.getDefaultToolkit().getImage("piece.gif"); 
-        
-        redcar1 = new ChessPiece("è½¦", bc, Color.red, this, w-4, h-4);
-        redcar1.setChessPiece(Redcolor);
-        redcar2 = new ChessPiece("è½¦", bc, Color.red, this, w-4, h-4);
-        redcar2.setChessPiece(Redcolor);
+public class ChessBoard extends JPanel implements MouseListener{
 
-        redhorse1 = new ChessPiece("é©¬", bc, Color.red, this, w-4, h-4);
-        redhorse1.setChessPiece(Redcolor);
-        redhorse2 = new ChessPiece("é©¬", bc, Color.red, this, w-4, h-4);
-        redhorse2.setChessPiece(Redcolor);
-        redcannon1 = new ChessPiece("ç‚®", bc, Color.red, this, w-4, h-4);
-        redcannon1.setChessPiece(Redcolor);
-        redcannon2 = new ChessPiece("ç‚®", bc, Color.red, this, w-4, h-4);
-        redcannon2.setChessPiece(Redcolor);
-        redelephant1 = new ChessPiece("ç›¸", bc, Color.red, this, w-4, h-4);
-        redelephant1.setChessPiece(Redcolor);
-        redelephant2 = new ChessPiece("ç›¸", bc, Color.red, this, w-4, h-4);
-        redelephant2.setChessPiece(Redcolor);
-        redgeneral = new ChessPiece("å¸…", bc, Color.red, this, w-4, h-4);
-        redgeneral.setChessPiece(Redcolor);
-        redofficial1 = new ChessPiece("ä»•", bc, Color.red, this, w-4, h-4);
-        redofficial1.setChessPiece(Redcolor);
-        redofficial2 = new ChessPiece("ä»•", bc, Color.red, this, w-4, h-4);
-        redofficial2.setChessPiece(Redcolor);
-        redsoldier1 = new ChessPiece("å…µ", bc, Color.red, this, w-4, h-4);
-        redsoldier1.setChessPiece(Redcolor);
-        redsoldier2 = new ChessPiece("å…µ", bc, Color.red, this, w-4, h-4);
-        redsoldier2.setChessPiece(Redcolor);
-        redsoldier3 = new ChessPiece("å…µ", bc, Color.red, this, w-4, h-4);
-        redsoldier3.setChessPiece(Redcolor);
-        redsoldier4 = new ChessPiece("å…µ", bc, Color.red, this, w-4, h-4);
-        redsoldier4.setChessPiece(Redcolor);
-        redsoldier5 = new ChessPiece("å…µ", bc, Color.red, this, w-4, h-4);
-        redsoldier5.setChessPiece(Redcolor);
-        
-        blackcar1 = new ChessPiece("è½¦", bc, Color.black, this, w-4, h-4);
-        blackcar1.setChessPiece(BlackColor);
-        blackcar2 = new ChessPiece("è½¦", bc, Color.black, this, w-4, h-4);
-        blackcar2.setChessPiece(BlackColor);
-        blackhorse1 = new ChessPiece("é©¬", bc, Color.black, this, w-4, h-4);
-        blackhorse1.setChessPiece(BlackColor);
-        blackhorse2 = new ChessPiece("é©¬", bc, Color.black, this, w-4, h-4);
-        blackhorse2.setChessPiece(BlackColor);
-        blackcannon1 = new ChessPiece("ç‚®", bc, Color.black, this, w-4, h-4);
-        blackcannon1.setChessPiece(BlackColor);
-        blackcannon2 = new ChessPiece("ç‚®", bc, Color.black, this, w-4, h-4);
-        blackcannon2.setChessPiece(BlackColor);
-        blackelephant1 = new ChessPiece("è±¡", bc, Color.black, this, w-4, h-4);
-        blackelephant1.setChessPiece(BlackColor);
-        blackelephant2 = new ChessPiece("è±¡", bc, Color.black, this, w-4, h-4);
-        blackelephant2.setChessPiece(BlackColor);
-        blackgeneral = new ChessPiece("å°†", bc, Color.black, this, w-4, h-4);
-        blackgeneral.setChessPiece(BlackColor);
-        blackofficial1 = new ChessPiece("ä»•", bc, Color.black, this, w-4, h-4);
-        blackofficial1.setChessPiece(BlackColor);
-        blackofficial2 = new ChessPiece("ä»•", bc, Color.black, this, w-4, h-4);
-        blackofficial2.setChessPiece(BlackColor);
-        blacksoldier1 = new ChessPiece("å’", bc, Color.black, this, w-4, h-4);
-        blacksoldier1.setChessPiece(BlackColor);
-        blacksoldier2 = new ChessPiece("å’", bc, Color.black, this, w-4, h-4);
-        blacksoldier2.setChessPiece(BlackColor);
-        blacksoldier3 = new ChessPiece("å’", bc, Color.black, this, w-4, h-4);
-        blacksoldier3.setChessPiece(BlackColor);
-        blacksoldier4 = new ChessPiece("å’", bc, Color.black, this, w-4, h-4);
-        blacksoldier4.setChessPiece(BlackColor);
-        blacksoldier5 = new ChessPiece("å’", bc, Color.black, this, w-4, h-4);
-        blacksoldier5.setChessPiece(BlackColor);
-        point[1][10].setPiece(redcar1, this);  
-        point[2][10].setPiece(redhorse1, this);  
-        point[3][10].setPiece(redelephant1, this);  
-        point[4][10].setPiece(redofficial1, this);  
-        point[5][10].setPiece(redgeneral, this);  
-        point[6][10].setPiece(redofficial2, this);  
-        point[7][10].setPiece(redelephant2, this);  
-        point[8][10].setPiece(redhorse2, this);  
-        point[9][10].setPiece(redcar2, this);  
-        point[2][8].setPiece(redcannon1, this);  
-        point[8][8].setPiece(redcannon2, this);  
-        point[1][7].setPiece(redsoldier1, this);  
-        point[3][7].setPiece(redsoldier2, this);  
-        point[5][7].setPiece(redsoldier3, this);  
-        point[7][7].setPiece(redsoldier4, this);  
-        point[9][7].setPiece(redsoldier5, this);  
-  
-        point[1][1].setPiece(blackcar1, this);  
-        point[2][1].setPiece(blackhorse1, this);  
-        point[3][1].setPiece(blackelephant1, this);  
-        point[4][1].setPiece(blackofficial1, this);  
-        point[5][1].setPiece(blackgeneral, this);  
-        point[6][1].setPiece(blackofficial2, this);  
-        point[7][1].setPiece(blackelephant2, this);  
-        point[8][1].setPiece(blackhorse2, this);  
-        point[9][1].setPiece(blackcar2, this);  
-        point[2][3].setPiece(blackcannon1, this);  
-        point[8][3].setPiece(blackcannon2, this);  
-        point[1][4].setPiece(blacksoldier1, this);  
-        point[3][4].setPiece(blacksoldier2, this);  
-        point[5][4].setPiece(blacksoldier3, this);  
-        point[7][4].setPiece(blacksoldier4, this);  
-        point[9][4].setPiece(blacksoldier5, this);  
-  
-        
-		
+	private int width;//ÆåÅÌÁ½ÏßÖ®¼äµÄ¾àÀë£»
+	boolean focus = false ; //Æå×ÓµÄ×´Ì¬£»
+	int jiang1_i = 4;//Ë§µÄi×ø±ê£»
+	int jiang1_j = 0;//Ë§µÄj×ø±ê£»
+	int jiang2_i = 4;//½«µÄi×ø±ê£»
+	int jiang2_j = 9;//½«µÄj×ø±ê£»
+	int startI = -1;//Æå×Ó¿ªÊ¼µÄÎ»ÖÃ£»
+	int startJ = -1;//
+	int endI = -1;//Æå×ÓµÄÖÕÖ¹Î»ÖÃ£»
+	int endJ = -1;
+	public ChessPiece chessPieces[][];//Æå×ÓµÄÊı×é£»
+	ChineseChess chineseChess= null;//ÉùÃ÷ÏóÆåµÄÒıÓÃ£»
+	ChineseRule chineseRule ;//ÉùÃ÷ÏóÆå¹æÔòµÄÒıÓÃ£»
+	public ChessBoard(ChessPiece chessPiece[][],int width,ChineseChess chineseChess){
+		this.chessPieces = chessPiece;
+		this.chineseChess = chineseChess;
+		this.width = width;
+		chineseRule = new ChineseRule(); 
+		this.addMouseListener(this);//ÎªÆåÅÌÌí¼ÓÊó±êÊÂ¼ş¼àÌıÆ÷£»
+		this.setBounds(0, 0, 700, 700);//ÉèÖÃÆåÅÌµÄ´óĞ¡£»
+		this.setLayout(null);//ÉèÖÃÎª¿Õ²¼¾Ö£»
 	}
-	
-	public void paintComponent(Graphics g){
-		super.paintComponent(g);
-		int imgWidth = img.getWidth(this);
-		int imgHight = img.getHeight(this);
-		int FWidth = getWidth();
-		int FHight = getHeight();
-		int x = (FWidth - imgWidth)/2;
-		int y = (FHight - imgHight)/2;
-		g.drawImage(img, x, y, null);
-		
-	}
-	@Override
-	public void mouseDragged(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-	@Override
-	public void mouseMoved(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
+	public void paint(Graphics g1){
+		Graphics2D g = (Graphics2D) g1;//»ñµÃGraphics2d¶ÔÏó£»
+		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);//´ò¿ª
+		Color color  = g.getColor();//»ñµÃ»­±ÊµÄÑÕÉ«£»
+		g.setColor(chineseChess.bgColor);
 	}
 	@Override
 	public void mouseClicked(MouseEvent e) {
 		// TODO Auto-generated method stub
 		
 	}
+
 	@Override
 	public void mousePressed(MouseEvent e) {
 		// TODO Auto-generated method stub
 		
 	}
+
 	@Override
 	public void mouseReleased(MouseEvent e) {
 		// TODO Auto-generated method stub
 		
 	}
+
 	@Override
 	public void mouseEntered(MouseEvent e) {
 		// TODO Auto-generated method stub
 		
 	}
+
 	@Override
 	public void mouseExited(MouseEvent e) {
 		// TODO Auto-generated method stub
 		
 	}
-	
 	
 }
--- ChineseChess/zhongjiawei/ChineseChess/src/com/zjw/chineseChess/MakeChessManual.java
+++
@@ -1,9 +0,0 @@
-package com.zjw.chineseChess;
-/**
- * 制作棋谱类
- * @author jiawei
- *
- */
-public class MakeChessManual {
-
-}
