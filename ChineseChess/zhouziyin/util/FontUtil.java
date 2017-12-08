52218250a55f03d97b383fe2bed6611afc0b952a
ChristmasChou <34117897+ChristmasChou@users.noreply.github.com>
2017-12-08 14:37:26 +0800

    Add files via upload

---
+++ ChineseChess/zhouziyin/util/FontUtil.java
@@ -0,0 +1,14 @@
+package com.zhouziyin.ChineseChess.util;
+
+import java.awt.*;
+
+/**
+ * Created by zhouziyin on 2017/12/8.
+ */
+public class FontUtil {
+
+    // 自定义字体
+    public static final Font myFont1 = new Font("华文新魏", Font.PLAIN, 16);
+    public static final Font myFont2 = new Font("华文新魏", Font.BOLD, 36);
+    public static final Font myFont3 = new Font("华文新魏", Font.BOLD, 72);
+}
