56d80789aafbd59b6ccd3078880bb294919fa954
jimmysenior <290084098@qq.com>
2017-12-09 22:03:18 +0800

    minjie finished the part of chinese chess gui,specifically,the main window and two player battle window

---
+++ ChineseChess/minjie/chinesechess/src/main/java/com/mj/maven/chinesechess/BattleToPlayerWindow.java
@@ -0,0 +1,140 @@
+package com.mj.maven.chinesechess;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.EventQueue;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.UIManager;
+import javax.swing.border.TitledBorder;
+
+public class BattleToPlayerWindow {
+
+	private JFrame battleToPlayerFrame;
+
+
+	private JPanel playerInfoPanel;
+	private JPanel chessBoardPanel;
+	private JPanel undoAndOptionsFatherPanel;
+	private JPanel undoAndOptionsChildPanel;
+
+	/**
+	 * Launch the application.
+	 */
+/*	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					BattleToPlayerWindow battleToPlayerWindow = new BattleToPlayerWindow();
+					battleToPlayerWindow.battleToPlayerFrame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}*/
+
+	/**
+	 * Create the application.
+	 */
+	public BattleToPlayerWindow() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		setBattleToPlayerFrame();
+		
+		addPlayerInfoPanel();
+		addPlayerOneInfoPanel();
+		addPlayerTwoInfoPanel();
+		
+		addChessBoardPanel();
+		addChessBoardBackground();
+		
+		addUndoAndOptionsFatherPanel();
+		addUndoAndOptionsChildPanel();
+		addUndoButton();
+		addOptionsButton();
+	}
+	
+	public JFrame getBattleToPlayerFrame() {
+		return battleToPlayerFrame;
+	}
+	
+	private void addChessBoardBackground() {
+		ImageIcon img=new ImageIcon("src/main/resources/images/chessboard.gif");
+		chessBoardPanel.setLayout(null);
+		JLabel imageLabel=new JLabel(img);
+		imageLabel.setBounds(0,0,img.getIconWidth(),img.getIconHeight());
+		chessBoardPanel.add(imageLabel);//注意这里是关键，将背景标签添加到jfram的LayeredPane面板里。
+	//	System.out.println(img.getIconWidth());
+	//	System.out.println(img.getIconHeight());
+		chessBoardPanel.setOpaque(false);
+	}
+	
+	private void addOptionsButton() {
+		JButton optionsButton = new JButton("选项");
+		undoAndOptionsChildPanel.add(optionsButton);		
+	}
+
+	private void addUndoButton() {
+		JButton undoButton = new JButton("悔棋");
+		undoAndOptionsChildPanel.add(undoButton);		
+	}
+
+	private void addUndoAndOptionsChildPanel() {
+		undoAndOptionsChildPanel = new JPanel();
+		undoAndOptionsFatherPanel.add(undoAndOptionsChildPanel, BorderLayout.SOUTH);
+		undoAndOptionsChildPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));		
+	}
+
+	private void addUndoAndOptionsFatherPanel() {
+		undoAndOptionsFatherPanel = new JPanel();
+		playerInfoPanel.add(undoAndOptionsFatherPanel);
+		undoAndOptionsFatherPanel.setLayout(new BorderLayout(0, 0));
+	}
+
+	private void addPlayerTwoInfoPanel() {
+		JPanel playerTwoInfoPanel = new JPanel();
+		playerTwoInfoPanel.setBorder(new TitledBorder(null, "Player 2", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		playerInfoPanel.add(playerTwoInfoPanel);
+		playerTwoInfoPanel.setLayout(new BorderLayout(0, 0));		
+	}
+
+	private void addPlayerOneInfoPanel() {
+		JPanel playerOneInfoPanel = new JPanel();
+		playerOneInfoPanel.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Player 1", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		playerInfoPanel.add(playerOneInfoPanel);
+		playerOneInfoPanel.setLayout(new BorderLayout(0, 0));
+	}
+
+	private void addChessBoardPanel() {
+		chessBoardPanel = new JPanel();
+		chessBoardPanel.setPreferredSize(new Dimension(558,620));//图片chessboard.gif的大小
+		battleToPlayerFrame.getContentPane().add(chessBoardPanel, BorderLayout.CENTER);
+	}
+
+	private void addPlayerInfoPanel() {
+		playerInfoPanel = new JPanel();
+		battleToPlayerFrame.getContentPane().add(playerInfoPanel, BorderLayout.WEST);
+		playerInfoPanel.setLayout(new GridLayout(0, 1, 0, 0));
+	}
+
+	private void setBattleToPlayerFrame() {
+		battleToPlayerFrame = new JFrame();
+		battleToPlayerFrame.setTitle("双人对战");
+		battleToPlayerFrame.setBounds(200, 50, 710, 660);
+		battleToPlayerFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		battleToPlayerFrame.getContentPane().setLayout(new BorderLayout(0, 0));	
+		battleToPlayerFrame.setResizable(false);
+	}
+}
