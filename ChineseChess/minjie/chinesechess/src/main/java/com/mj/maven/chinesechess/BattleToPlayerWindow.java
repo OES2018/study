6983410795cc21da79ec2b821619c3d60bf8ce25
jimmysenior <290084098@qq.com>
2017-12-10 11:57:40 +0800

    commit the work directory under the .gitignore

---
+++ ChineseChess/minjie/chinesechess/src/main/java/com/mj/maven/chinesechess/BattleToPlayerWindow.java
@@ -0,0 +1,151 @@
+package com.mj.maven.chinesechess;
+
+import java.awt.BorderLayout;
+
+import java.awt.Dimension;
+import java.awt.EventQueue;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.UIManager;
+import javax.swing.border.TitledBorder;
+
+/**
+*@auther minjie 
+*@E-mail jie_min_jimmy@163.com
+*@version 创建时间：2017年12月10日
+**/
+
+public class BattleToPlayerWindow {
+
+	private JFrame battleToPlayerFrame;
+
+	private JPanel playerInfoPanel;
+	private JPanel chessBoardPanel;
+	private JPanel undoAndOptionsFatherPanel;
+	private JPanel undoAndOptionsChildPanel;
+	
+	private int CHESS_BOARD_WEDTH=558;
+	private int CHESS_BOARD_HEIGHT=620;
+
+	/**
+	 * Launch the application.
+	 */
+/*	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					BattleToPlayerWindow battleToPlayerWindow = new BattleToPlayerWindow();
+					battleToPlayerWindow.battleToPlayerFrame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}*/
+
+	/**
+	 * Create the application.
+	 */
+	public BattleToPlayerWindow() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		setBattleToPlayerFrame();
+		
+		addPlayerInfoPanel();
+		addPlayerOneInfoPanel();
+		addPlayerTwoInfoPanel();
+		
+		
+		addChessBoardPanel();
+		addChessBoardBackground();//添加棋盘背景图
+		
+		addUndoAndOptionsFatherPanel();
+		addUndoAndOptionsChildPanel();
+		addUndoButton();
+		addOptionsButton();
+	}
+	
+	public JFrame getBattleToPlayerFrame() {
+		return battleToPlayerFrame;
+	}
+	
+	private void setBattleToPlayerFrame() {
+		battleToPlayerFrame = new JFrame();
+		battleToPlayerFrame.setTitle("双人对战");
+		battleToPlayerFrame.setBounds(200, 50, 710, 660);
+		battleToPlayerFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		battleToPlayerFrame.getContentPane().setLayout(new BorderLayout(0, 0));	
+		battleToPlayerFrame.setResizable(false);
+	}
+	
+	private void addPlayerInfoPanel() {
+		playerInfoPanel = new JPanel();
+		battleToPlayerFrame.getContentPane().add(playerInfoPanel, BorderLayout.WEST);
+		playerInfoPanel.setLayout(new GridLayout(0, 1, 0, 0));
+	}
+	
+	private void addPlayerOneInfoPanel() {
+		JPanel playerOneInfoPanel = new JPanel();
+		playerOneInfoPanel.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Player 1", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		playerInfoPanel.add(playerOneInfoPanel);
+		playerOneInfoPanel.setLayout(new BorderLayout(0, 0));
+	}
+	
+	private void addPlayerTwoInfoPanel() {
+		JPanel playerTwoInfoPanel = new JPanel();
+		playerTwoInfoPanel.setBorder(new TitledBorder(null, "Player 2", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		playerInfoPanel.add(playerTwoInfoPanel);
+		playerTwoInfoPanel.setLayout(new BorderLayout(0, 0));		
+	}
+	
+	private void addChessBoardPanel() {
+		chessBoardPanel = new JPanel();
+		chessBoardPanel.setPreferredSize(new Dimension(CHESS_BOARD_WEDTH,CHESS_BOARD_HEIGHT));
+		battleToPlayerFrame.getContentPane().add(chessBoardPanel, BorderLayout.CENTER);
+	}
+	
+	private void addChessBoardBackground() {
+		ImageIcon img=new ImageIcon("src/main/resources/images/chessboard.gif");
+		chessBoardPanel.setLayout(null);
+		JLabel imageLabel=new JLabel(img);
+		imageLabel.setBounds(0,0,img.getIconWidth(),img.getIconHeight());
+		chessBoardPanel.add(imageLabel);//注意这里是关键，将背景标签添加到jfram的LayeredPane面板里。
+	//	System.out.println(img.getIconWidth());
+	//	System.out.println(img.getIconHeight());
+		chessBoardPanel.setOpaque(false);
+	}
+	
+	private void addUndoAndOptionsFatherPanel() {
+		undoAndOptionsFatherPanel = new JPanel();
+		playerInfoPanel.add(undoAndOptionsFatherPanel);
+		undoAndOptionsFatherPanel.setLayout(new BorderLayout(0, 0));
+	}
+	
+	private void addUndoAndOptionsChildPanel() {
+		undoAndOptionsChildPanel = new JPanel();
+		undoAndOptionsFatherPanel.add(undoAndOptionsChildPanel, BorderLayout.SOUTH);
+		undoAndOptionsChildPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));		
+	}
+
+	private void addUndoButton() {
+		JButton undoButton = new JButton("悔棋");
+		undoAndOptionsChildPanel.add(undoButton);		
+	}
+
+	private void addOptionsButton() {
+		JButton optionsButton = new JButton("选项");
+		undoAndOptionsChildPanel.add(optionsButton);		
+	}
+
+}
---
+++ ChineseChess/minjie/chinesechess/src/main/java/com/mj/maven/chinesechess/ChineseChessMainWindow.java
@@ -0,0 +1,148 @@
+package com.mj.maven.chinesechess;
+
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+/**
+*@auther minjie 
+*@E-mail jie_min_jimmy@163.com
+*@version 创建时间：2017年12月10日
+**/
+
+public class ChineseChessMainWindow {
+
+	private JFrame mainFrame;
+	private JPanel gameModeChoosenPanel;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ChineseChessMainWindow mainWindow = new ChineseChessMainWindow();
+					mainWindow.mainFrame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public ChineseChessMainWindow() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		setMainFrame();
+		
+		//添加主界面的背景图
+		addBackground();
+		
+		//添加游戏选项
+		addGameModeChoosenPanel();
+		addBattelToComputerButton();
+		addBattelToPlayerButton();
+		addSetUpButton();
+		addHelpButton();
+	}
+
+	private void setMainFrame() {
+		mainFrame = new JFrame();
+		mainFrame.setTitle("中国象棋v1.0");
+		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		mainFrame.getContentPane().setLayout(null);
+		mainFrame.setBounds(200, 50, 1100, 750);
+		mainFrame.setResizable(false);
+	}
+	
+	private void addBackground() {
+		ImageIcon img=new ImageIcon("src/main/resources/images/background.jpg");
+		JLabel imageLabel=new JLabel(img);
+		imageLabel.setBounds(0,0,img.getIconWidth(),img.getIconHeight());
+		mainFrame.getLayeredPane().add(imageLabel,new Integer(Integer.MIN_VALUE));//注意这里是关键，将背景标签添加到jfram的LayeredPane面板里。
+	//	System.out.println(img.getIconWidth());
+	//	System.out.println(img.getIconHeight());
+		Container cp=mainFrame.getContentPane();
+		((JPanel)cp).setOpaque(false);
+	}
+
+	private void addGameModeChoosenPanel() {
+		gameModeChoosenPanel = new JPanel();
+		gameModeChoosenPanel.setOpaque(false);
+		gameModeChoosenPanel.setBounds(53, 428, 216, 244);
+		mainFrame.getContentPane().add(gameModeChoosenPanel);
+		gameModeChoosenPanel.setLayout(new GridLayout(4, 1, 0, 0));
+	}
+	
+	private void addBattelToComputerButton() {
+		JButton battelToComputerButton = new JButton("对战电脑");
+		battelToComputerButton.setFont(new Font("宋体", Font.BOLD, 18));
+		battelToComputerButton.setBackground(Color.LIGHT_GRAY);
+		battelToComputerButton.setOpaque(false);
+		battelToComputerButton.setBorder(null);
+		gameModeChoosenPanel.add(battelToComputerButton);
+	}
+	
+	private void addBattelToPlayerButton() {
+		JButton battelToPlayerButton = new JButton("双人对战");
+		battelToPlayerButton.setFont(new Font("宋体", Font.BOLD, 18));
+		battelToPlayerButton.setBackground(Color.LIGHT_GRAY);
+		battelToPlayerButton.setOpaque(false);
+		battelToPlayerButton.setBorder(null);
+		battelToPlayerButton.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				EventQueue.invokeLater(new Runnable() {
+					public void run() {
+						try {
+							BattleToPlayerWindow battleToPlayerwindow = new BattleToPlayerWindow();
+							battleToPlayerwindow.getBattleToPlayerFrame().setVisible(true);
+							mainFrame.setVisible(false);
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+					}
+				});				
+			}
+		});
+		gameModeChoosenPanel.add(battelToPlayerButton);		
+	}
+	
+	private void addSetUpButton() {
+		JButton setUpButton = new JButton("设置");
+		setUpButton.setFont(new Font("宋体", Font.BOLD, 18));
+		setUpButton.setBackground(Color.LIGHT_GRAY);
+		setUpButton.setOpaque(false);
+		setUpButton.setBorder(null);
+		gameModeChoosenPanel.add(setUpButton);		
+	}
+	
+	private void addHelpButton() {
+		JButton helpButton = new JButton("帮助");
+		helpButton.setFont(new Font("宋体", Font.BOLD, 18));
+		helpButton.setBackground(Color.LIGHT_GRAY);
+		helpButton.setOpaque(false);
+		helpButton.setBorder(null);
+		gameModeChoosenPanel.add(helpButton);		
+	}
+
+}
